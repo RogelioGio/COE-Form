56057e26a8275fe2552f03cdfa3a4448885c4b36
RogelioGio <giotalingdan@gmail.com>
2026-02-06 10:10:38 +0800

    Validation and New field Added

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import { Paperclip, FileIcon, X } from "lucide-react"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [submitting, setSubmitting] = useState(false);
 
   const SPARef = useRef(null);
   const [spa, setSPA] = useState({});
@@ -47,7 +48,8 @@ function App() {
       }),
       Issue_On: Yup.string().required('Issue On is required'),
       Issue_at: Yup.string().required('Issue at is required'),
-      SPA_Authorization: Yup.string().required('SPA or Authorization is required'),
+      LRA_Official_ID: Yup.string().required('LRA Official ID is required'),
+      ID_Number: Yup.string().required('ID Number is required').max(50, 'ID Number must be at most 50 characters'),
     }),
     onSubmit: values => {
       formik.setFieldValue("Timestamp", new Date().toLocaleString());
@@ -139,13 +141,39 @@ function App() {
                 <p className='text-sm text-gray-500'>Required</p>
               </div>
               <div className='flex flex-row text-black justify-between w-full gap-4'>
-                <div className={`flex flex-row items-center justify-center py-3 w-full rounded-lg border-gray-600 border hover:text-white hover:bg-gray-800 transition-all ease-in-out cursor-pointer ${formik.values.Data_Owner === "Yes" ? 'bg-gray-800 text-white' : ''}`}
+                {/* <div className={`flex flex-row items-center justify-center py-3 w-full rounded-lg border-gray-600 border hover:text-white hover:bg-gray-800 transition-all ease-in-out cursor-pointer ${formik.values.Data_Owner === "Yes" ? 'bg-gray-800 text-white' : ''}`}
                   onClick={() => formik.setFieldValue("Data_Owner", "Yes")}>
                     <p>Yes</p>
                 </div>
                 <div className={`flex flex-row items-center justify-center py-3 w-full rounded-lg border-gray-600 border hover:text-white hover:bg-gray-800 transition-all ease-in-out cursor-pointer ${formik.values.Data_Owner === "No" ? 'bg-gray-800 text-white' : ''}`}  
                   onClick={() => formik.setFieldValue("Data_Owner", "No")}>
                     <p>No</p>
+                </div> */}
+                <div className='flex flex-row w-full justify-around'>
+                  <div>
+                    <input
+                      type="radio"
+                      id="dataOwnerYes"
+                      name="Data_Owner"
+                      value="Yes"
+                      checked={formik.values.Data_Owner === "Yes"}
+                      onChange={formik.handleChange}
+                      className='mr-2'
+                    />
+                    <label htmlFor="dataOwnerYes">Yes</label>
+                  </div>
+                  <div>
+                    <input
+                      type="radio"
+                      id="dataOwnerNo"
+                      name="Data_Owner"
+                      value="No"
+                      checked={formik.values.Data_Owner === "No"}
+                      onChange={formik.handleChange}
+                      className='mr-2'
+                    />
+                    <label htmlFor="dataOwnerNo">No</label>
+                  </div>
                 </div>
               </div>
               {formik.touched.Data_Owner && formik.errors.Data_Owner ? (
@@ -188,7 +216,7 @@ function App() {
                   value={formik.values.Relation}
                 />
               </div>
-              {formik.errors.Relation && formik.errors.Relation ? (
+              {formik.touched.Relation && formik.errors.Relation ? (
                   <p className="text-sm text-red-600">{formik.errors.Relation}</p>
                 ) : null}
               </>  
@@ -217,13 +245,13 @@ function App() {
                         <Calendar
                         mode="single"
                         selected={formik.values.Issue_On ? new Date(formik.values.Issue_On) : undefined}
-                        onSelect={(date) => formik.setFieldValue("Issue_On", date.toISOString())}
+                        onSelect={(date) => formik.setFieldValue("Issue_On", date)}
                         className="rounded-lg border"
                         captionLayout="dropdown"
                       />
                       </PopoverContent>
                     </Popover>
-                    {formik.errors.Issue_On ? (
+                    {formik.touched.Issue_On && formik.errors.Issue_On ? (
                     <p className="text-sm text-red-600">{formik.errors.Issue_On}</p>
                   ) : null}
                   </div>
@@ -243,43 +271,46 @@ function App() {
                     onChange={formik.handleChange}
                     value={formik.values.Issue_at}
                   />
-                    {formik.errors.Issue_at ? (
+                    {formik.touched.Issue_at && formik.errors.Issue_at ? (
                     <p className="text-sm text-red-600">{formik.errors.Issue_at}</p>
                   ) : null}
                 </div>
             </div>
             <div className='flex flex-row w-full gap-4'>
-              <div className='w-full'>
-                  <div className='flex flex-row w-full justify-between'>
-                    <label className='font-text block text-sm font-medium text-gray-700 text-left mb-2' htmlFor="SPA_Authorization">SPA or Authorization</label>
-                    <p className='text-sm text-gray-500'>Required</p>
-                  </div>
-                <input type="file" ref={SPARef} className='hidden' onChange={handleFileChange} id='SPA_Authorization'/>
-                <div onClick={handleFileClick} className="p-10 border-dashed border-2 cursor-pointer rounded-2xl flex flex-row justify-center items-center hover:bg-gray-100 transition-all ease-in-out border-gray-300">
-                  <FileIcon className='mx-auto mb-2' />
-                  {
-                    spa.name ? (
-                      <div className='text-center'>
-                        <p className='font-text font-medium'>{spa.name}</p>
-                        <p className='text-sm text-gray-500'>{(spa.size / 1024).toFixed(2)} KB</p>
+              {
+                formik.values.Data_Owner === "No" ? (
+                  <div className='w-full'>
+                      <div className='flex flex-row w-full justify-between'>
+                        <label className='font-text block text-sm font-medium text-gray-700 text-left mb-2' htmlFor="SPA_Authorization">SPA or Authorization</label>
                       </div>
-                    ) : (
-                      <p className='text-center text-gray-500 text-sm'>Click to upload SPA Authorization</p>
-                    )
-                  }
-                </div>
-                              {formik.touched.Data_Owner && formik.errors.Data_Owner ? (
-                <p className="text-sm text-red-600">{formik.errors.Data_Owner}</p>
-              ) : null}
-              </div>
+                    <input type="file" ref={SPARef} className='hidden' onChange={handleFileChange} id='SPA_Authorization'/>
+                    <div onClick={handleFileClick} className="p-10 border-dashed border-2 cursor-pointer rounded-2xl flex flex-row justify-center items-center hover:bg-gray-100 transition-all ease-in-out border-gray-300">
+                      <FileIcon className='mx-auto mb-2' />
+                      {
+                        spa.name ? (
+                          <div className='text-center'>
+                            <p className='font-text font-medium'>{spa.name}</p>
+                            <p className='text-sm text-gray-500'>{(spa.size / 1024).toFixed(2)} KB</p>
+                          </div>
+                        ) : (
+                          <p className='text-center text-gray-500 text-sm'>Click to upload SPA Authorization</p>
+                        )
+                      }
+                    </div>
+                    {formik.touched.Data_Owner && formik.errors.Data_Owner ? (
+                    <p className="text-sm text-red-600">{formik.errors.Data_Owner}</p>
+                  ) : null}
+                  </div>
+                ) : null
+              }
               <div className='w-full'>
                     <div className='flex flex-row w-full justify-between'>
                       <label className='font-text block text-sm font-medium text-gray-700 text-left mb-2' htmlFor="LRA_Official_ID">LRA Official ID</label>
                       <p className='text-sm text-gray-500'>Required</p>
                   </div>
                   <input type="file" ref={LRAIdRef} className='hidden' onChange={handleLRAIdChange} id='LRA_Official_ID'/>
-                  <div onClick={uploadLRAIdClick} className="p-10 border-dashed border-2 cursor-pointer rounded-2xl flex flex-row justify-center items-center hover:bg-gray-100 transition-all ease-in-out border-gray-300">
-                  <FileIcon className='mx-auto mb-2' />
+                  <div onClick={uploadLRAIdClick} className="gap-2 p-10 border-dashed border-2 cursor-pointer rounded-2xl flex flex-row justify-center items-center hover:bg-gray-100 transition-all ease-in-out border-gray-300">
+                  <FileIcon className= {``} />
                   {
                     LRAId.name ? (
                       <div className='text-center'>
@@ -291,10 +322,31 @@ function App() {
                     )
                   }
                 </div>
+                  {formik.touched.LRA_Official_ID && formik.errors.LRA_Official_ID ? (
+                    <p className="text-sm text-red-600">{formik.errors.LRA_Official_ID}</p>
+                  ) : null}
               </div>
             </div>
-            
-
+            <div>
+              <div className='flex flex-row justify-between'>
+                <label className='font-text block text-sm font-medium text-gray-700 text-left mb-2' htmlFor="ID_Number">ID Number</label>
+                <p className='text-sm text-gray-500'>Required</p>
+              </div>
+              <input
+                className='border text-black border-gray-300 rounded-md p-2 w-full '
+                id="ID_Number"
+                name="ID_Number"
+                type="text"
+                placeholder='00000000000'
+                maxLength="50"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.ID_Number}
+              />
+              {formik.touched.ID_Number && formik.errors.ID_Number ? (
+                <p className="text-sm text-red-600">{formik.errors.ID_Number}</p>
+              ) : null}
+            </div>
             <button type='submit' className='w-full mt-5 bg-blue-600 text-white font-text px-4 py-2 rounded-md hover:bg-blue-700' onClick={() => formik.setFieldValue("Timestamp", new Date().toLocaleString())}>Submit</button>
           </form>
         </div>
